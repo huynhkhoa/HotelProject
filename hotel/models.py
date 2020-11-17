5b2161a0f5e6ef4430b47690c27675d46565b4be
huynhkhoa <huynhkhoa11012015@gmail.com>
2020-11-17 21:41:53 +0700

    tao moi quan he database

--- hotel/models.py
+++ hotel/models.py
@@ -1,9 +1,9 @@
 from sqlalchemy import Column, Integer, Float, \
-    String, ForeignKey, Date
-from datetime import datetime
-from sqlalchemy.orm import relationship
-
+    String, ForeignKey, Date, Boolean
 from hotel import db
+from sqlalchemy.orm import relationship
+from datetime import datetime
+from flask_login import UserMixin
 
 
 class Type(db.Model):
@@ -15,36 +15,43 @@ class Type(db.Model):
 
     roomdetails = relationship('RoomDetail', backref='type', lazy=True)
 
+    def __str__(self):
+        return self.name
+
 
 class RoomDetail(db.Model):
     __tablename__ = "roomdetail"
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(50), nullable=False)
     price = Column(Float, default=0)
-    bed = Column(Integer, nullable=False)
-    size = Column(String(50), nullable=False)
     max_guess = Column(Integer, nullable=True)
     description = Column(String(255), nullable=True)
 
     type_id = Column(Integer, ForeignKey(Type.id), nullable=False)
     bookings = relationship('Booking', backref='type', lazy=True)
 
+    def __str__(self):
+        return self.name
 
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     __tablename__ = "user"
+
     id = Column(Integer, primary_key=True, autoincrement=False)
-    first_name = Column(String(50), nullable=False)
-    last_name = Column(String(50), nullable=False)
+    name = Column(String(50), nullable=False)
     email = Column(String(50), nullable=False)
     phone = Column(Integer, nullable=False)
-    DoB = Column(Date, nullable=False)
-    foregin = Column(String(50), nullable=False)
+    identity_card = Column(Integer, nullable=False)
+    foregin = Column(Boolean, nullable=False)
+    active = Column(Boolean, default=True)
     username = Column(String(50), nullable=False)
     password = Column(String(50), nullable=False)
 
     orders = relationship('Order', backref='type', lazy=True)
+    invoices = relationship('Invoice', backref='type', lazy=True)
 
+    def __str__(self):
+        return self.name
 
 
 class Order(db.Model):
@@ -55,8 +62,10 @@ class Order(db.Model):
     noguess = Column(Integer, nullable=False)
     NoRoom = Column(Integer, nullable=False)
 
-    bookings = relationship('Booking', backref='type', lazy=True)
     user_id = Column(Integer, ForeignKey(User.id), nullable=False)
+
+    def __str__(self):
+        return self.name
 
 
 class Booking(db.Model):
@@ -65,7 +74,20 @@ class Booking(db.Model):
     roomdetail_id = Column(Integer, ForeignKey(RoomDetail.id), nullable=False)
     order_id = Column(Integer, ForeignKey(Order.id), nullable=False)
 
+    def __str__(self):
+        return self.name
 
+
+class Invoice(db.Model):
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    joined_date = Column(Date, default=datetime.now())
+
+    order_id = Column(Integer, ForeignKey(Order.id), nullable=False)
+
+    user_id = Column(Integer, ForeignKey(User.id), nullable=False)
+
+    def __str__(self):
+        return self.name
 
 
 if __name__ == "__main__":
