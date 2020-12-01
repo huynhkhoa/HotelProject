aabe2f5b667bb405be9dfcf307c1fc4634fcb66c
huynhkhoa <huynhkhoa11012015@gmail.com>
2020-12-01 17:03:25 +0700

    update

--- hotel/__init__.py
+++ hotel/__init__.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 app.secret_key = "\xb9\xd5\x97\xcc4\x81\xf8X\x98N\xde\xd6\x07?\xf5\x9f"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:0000@localhost/hoteldb?charset=utf8mb4'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['ROOT_PROJECT_PATH'] = app.root_path
 
 db = SQLAlchemy(app=app)
 
