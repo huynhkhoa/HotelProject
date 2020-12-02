1015091046f5f0039405fbf4fc818422ff625f16
huynhkhoa <huynhkhoa11012015@gmail.com>
2020-12-02 18:42:40 +0700

    update

--- hotel/admin.py
+++ hotel/admin.py
@@ -12,62 +12,62 @@ class AuthenticatedView(ModelView):
         return current_user.is_authenticated
 
 
-class LogoutView(BaseView):
+class LogoutView(BaseView):              # ke thua LogoutView
     @expose("/")
     def index(self):
         logout_user()
 
         return redirect("/")
 
-    def is_accessible(self):
+    def is_accessible(self):             # login thanh cong thi show LogoutView
         return current_user.is_authenticated
 
 
-class ContactView(BaseView):
+class ContactView(BaseView):             # ke thua ContactView
     @expose('/')
     def index(self):
         return self.render('admin/contact.html')
 
-    def is_accessible(self):
+    def is_accessible(self):             # login thanh cong thi show ContactView
         return current_user.is_authenticated
 
 
-class TypeModelView(ModelView):
+class TypeModelView(ModelView):          # ke thua TypeModelView
     column_display_pk = True
     can_export = True
 
-    def is_accessible(self):
+    def is_accessible(self):             # login thanh cong thi show TypeModelView
         return current_user.is_authenticated
 
 
-class RoomDetailModelView(ModelView):
+class RoomDetailModelView(ModelView):    # ke thua RoomDetailModelView
     can_export = True
 
-    def is_accessible(self):
+    def is_accessible(self):             # login thanh cong thi show RoomDetailModelView
         return current_user.is_authenticated
 
 
-class UserModelView(ModelView):
+class UserModelView(ModelView):          # ke thua UserModelView
     column_display_pk = True
     can_export = True
 
-    def is_accessible(self):
+    def is_accessible(self):             # login thanh cong thi show UserModelView
         return current_user.is_authenticated
 
 
-class OrderModelView(ModelView):
+class OrderModelView(ModelView):          # ke thua OrderModelView
     column_display_pk = True
     can_export = True
 
-    def is_accessible(self):
+    def is_accessible(self):              # login thanh cong thi show OrderModelView
         return current_user.is_authenticated
 
 
-class BookingModelView(ModelView):
+class BookingModelView(ModelView):         # ke thua BookingModelView
     column_display_pk = True
     can_export = True
 
-    def is_accessible(self):
+    def is_accessible(self):               # login thanh cong thi show BookingModelView
         return current_user.is_authenticated
 
 
@@ -75,10 +75,11 @@ class InvoiceModelView(ModelView):
     column_display_pk = True
     can_export = True
 
-    def is_accessible(self):
+    def is_accessible(self):                # login thanh cong thi show InvoiceModelView
         return current_user.is_authenticated
 
 
+#add cac class vao page admin
 admin.add_view(RoomDetailModelView(RoomDetail, db.session))
 admin.add_view(TypeModelView(Type, db.session))
 admin.add_view(UserModelView(User, db.session))
