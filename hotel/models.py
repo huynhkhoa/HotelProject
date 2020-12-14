977a0cbeac2a04d89ff6b7f895d8d48a1ed78951
huynhkhoa <huynhkhoa11012015@gmail.com>
2020-12-14 18:44:01 +0700

    update all database

--- hotel/models.py
+++ hotel/models.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, Float, \
-    String, ForeignKey, Date, Boolean, Enum
-from hotel import db
+    String, ForeignKey, Date, Boolean, Enum, DateTime
+from hotel import db, Status, CustomerType
 from sqlalchemy.orm import relationship
 from datetime import datetime
 from flask_login import UserMixin
@@ -11,22 +11,12 @@ class Type(db.Model):
     __tablename__ = "type"
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(50), nullable=False)
+    price = Column(Float, default=0)
     note = Column(String(255), nullable=True)
-
     roomdetails = relationship('RoomDetail', backref='type', lazy=True)
 
     def __str__(self):
-        return self.name
-
-
-class Booking(db.Model):
-    __tablename__ = "booking"
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    orders = relationship('Order', backref='booking', lazy=True)
-    roomdetails = relationship('RoomDetail', backref='booking', lazy=True)
-
-    def __str__(self):
-        return self.name
+        return self.name + " - Giá: " + self.price.__str__()
 
 
 class RoomDetail(db.Model):
@@ -34,15 +24,28 @@ class RoomDetail(db.Model):
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(50), nullable=False)
     price = Column(Float, default=0)
-    max_guess = Column(Integer, nullable=True)
-    description = Column(String(500), nullable=True)
+    guess = Column(Integer, nullable=True)
+    status = Column(Enum(Status), nullable=True)
     image = Column(String(255))
+    description = Column(String(500), nullable=True)
     type_id = Column(Integer, ForeignKey(Type.id), nullable=False)
-    booking_id = Column(Integer, ForeignKey(Booking.id))
+    bookings = relationship("Booking", backref='roomdetail', uselist=False)
+    orders = relationship("Order", backref='roomdetail', lazy=True)
 
-    @property
     def __str__(self):
         return self.name
+
+
+class Customer(db.Model):
+    __tablename__ = "Customer"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    customer_name = Column(String(50), nullable=False)
+    coefficient = Column(Float, nullable=False)
+    note = Column(String(50), nullable=False)
+    orders = relationship('Order', backref="customer", lazy=True)
+
+    def __str__(self):
+        return self.customer_name
 
 
 class UserRole(UserEnum):
@@ -57,7 +60,6 @@ class User(db.Model, UserMixin):
     name = Column(String(50), nullable=False)
     email = Column(String(50), nullable=False)
     phone = Column(Integer, nullable=False)
-    identity_card = Column(Integer, nullable=False)
     active = Column(Boolean, default=True)
     username = Column(String(50), nullable=False)
     password = Column(String(50), nullable=False)
@@ -70,18 +72,39 @@ class User(db.Model, UserMixin):
         return self.name
 
 
+class Surcharge(db.Model):
+    __tablename__ = "surcharge"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    surcharge = Column(Integer, nullable=False)
+    quantity = Column(Integer, nullable=False)
+    orders = relationship('Order', backref="surcharge", lazy=True)
+
+    def __str__(self):
+        return self.quantity.__str__() + " Người - " + self.surcharge.__str__() + " %"
+
+
 class Order(db.Model):
     __tablename__ = "order"
     id = Column(Integer, primary_key=True, autoincrement=True)
-    date_arrive = Column(Date, default=datetime.now())
-    NoDay = Column(Integer, nullable=False)
-    Noguess = Column(Integer, nullable=False)
-    NoRoom = Column(Integer, nullable=False)
-    foregin = Column(Boolean, nullable=False)
-
+    name = Column(String(50), nullable=False)
+    identity_card = Column(Integer, nullable=False)
+    address = Column(String(50), nullable=False)
+    created_date = Column(DateTime, default=datetime.today())
+    customer_id = Column(Integer, ForeignKey(Customer.id), nullable=False)
+    roomdetail_id = Column(Integer, ForeignKey(RoomDetail.id), nullable=False)
+    surcharge_id = Column(Integer, ForeignKey(Surcharge.id), nullable=False)
     user_id = Column(Integer, ForeignKey(User.id), nullable=False)
-    booking_id = Column(Integer, ForeignKey(Booking.id), nullable=False)
-    invoice = relationship("Invoice", backref='order', uselist=False)
+    invoices = relationship("Invoice", backref='order', uselist=False)
+
+    def __str__(self):
+        return self.id.__str__() + " - Khách hàng: " + self.name.__str__() + " - CMND: " + self.identity_card.__str__()
+
+
+class Booking(db.Model):
+    __tablename__ = "booking"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    order_id = Column(Integer, ForeignKey(Order.id), nullable=False)
+    roomdetail_id = Column(Integer, ForeignKey(RoomDetail.id), nullable=False)
 
     def __str__(self):
         return self.name
@@ -90,8 +113,12 @@ class Order(db.Model):
 class Invoice(db.Model):
     __tablename__ = "invoice"
     id = Column(Integer, primary_key=True, autoincrement=True)
-    joined_date = Column(Date, default=datetime.now())
+    date = Column(Integer, nullable=False, default=0)
+    price = Column(Float, default=0)
+    value = Column(Integer, nullable=False, default=0)
     order_id = Column(Integer, ForeignKey(Order.id), nullable=False)
+    user_id = Column(Integer, ForeignKey(User.id), nullable=False)
+    quantity = Column(Integer, default=0)
 
     def __str__(self):
         return self.name
